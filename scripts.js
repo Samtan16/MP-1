fba45a878d7298ee996fda1d16a05af6bbbd7abd
samuel tan <delta16@outlook.sg>
2022-12-10 11:02:28 +0000

    Redesigned Recipes

--- scripts.js
+++ scripts.js
@@ -1,116 +1,135 @@
-$(function () {
-  var anim_id;
-  
-  //DOM objects to variables
-  var container = $('#container');
-  var ball = $('#ball');
-  var paddle = $('.paddle');
-  var paddle1 = $('#paddle1');
-  var paddle2 = $('#paddle2');
-  var restart_btn = $('#restart_btn');
-  
-  //setup
-  var container_width = parseInt(container.width());
-  var container_height = parseInt(container.height());
-  var paddle_width = parseInt(paddle.width());
-  var ball_height = parseInt(ball.height());
-  var ball_width = parseInt(ball.width());
-  
-  //declarations
-  var game_over = false;
-  
-  var ball_center;
-  var paddle_center;
-  
-  var ball_go = 'down';
-  var ball_right_left = 'right';
-  
-  var top = 6;
-  var right_left_angle = 0;
-  
-  var move_right_p1 = false;
-  var move_left_p1 = false;
-  
-  var move_right_p2 = false;
-  var move_left_p2 = false;
-  
-  var who_won;
-  
-  //game code starts
-  
-  $(document).on('keydown', function (e) {
-    var key = e.keyCode;
-    if (key === 37 && move_left_p1 === false) {
-      move_left_p1 = requestAnimationFrame(left_p1);
+// JavaScript for tp7
+
+// function to load a file from the URL "fromFile" into the object indentified by "whereTo"
+function loadFileInto(fromFile, whereTo) {
+
+  // creating a new XMLHttpRequest object
+  ajax = new XMLHttpRequest();
+
+  // defines the GET/POST method, source, and async value of the AJAX object
+  ajax.open("GET", fromFile, true);
+
+  // provides code to do something in response to the AJAX request
+  ajax.onreadystatechange = function() {
+    if ((this.readyState == 4) && (this.status == 200)) {
+      document.querySelector(whereTo).innerHTML = this.responseText;
+
+    } else if ((this.readyState == 4) && (this.status != 200)) {
+      console.log("Error: " + this.responseText);
     }
-  });
+
+  } // end ajax.onreadystatechange function
+
+  // initiate request and wait for response
+  ajax.send();
+
+}
+
+//new recipe object
+function Recipe(recipeName, contributorName, imageURL, ingredientsURL, equipmentURL, directionsURL) {
   
-  $(document).on('keyup', function (e) {
-    var key = e.keyCode;
-    if (key === 37) {
-      cancelAnimationFrame(move_left_p1);
-      move_left_p1 = false;
-    }
-  });
+  this.recipeName = recipeName;
+  this.contributor = contributorName;
+  this.imageURL = imageURL;
+  this.ingredients = ingredientsURL;
+  this.equipment = equipmentURL;
+  this.directions = directionsURL;
   
-  function left_p1() {
+  this.displayRecipe = function() {
     
-    paddle1.css('left', parseInt(paddle1.css('left')) - 15);
-    move_left_p1 = requestAnimationFrame(left_p1); 
- }
-  
-  
-  
-  //game code ends
-  
-  function collision($div1, $div2) {
-    var x1 = $div1.offset().left;
-    var y1 = $div1.offset().top;
-    var h1 = $div1.outerHeight(true);
-    var w1 = $div1.outerWidth(true);
-    var b1 = y1 + h1;
-    var r1 = x1 + w1;
-    var x2 = $div2.offset().left;
-    var y2 = $div2.offset().top;
-    var h2 = $div2.outerHeight(true);
-    var w2 = $div2.outerWidth(true);
-    var b2 = y2 + h2;
-    var r2 = x2 + w2;
-    
-    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-    return true;
-    
+    document.querySelector("#header h1").innerHTML = this.recipeName;
+    document.querySelector("#contributor").innerHTML = this.contributor;
+    document.querySelector("#header").style.backgroundImage = "url(" + this.imageURL + ")";
+    loadFileInto(this.ingredients, "#ingredients ul");
+    loadFileInto(this.equipment, "#equipment ul");
+    loadFileInto(this.directions, "#directions ol");
   }
+ 
+}
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-});
+  LemonPankoCrustedSalmon = new Recipe(
+    "Lemon Panko Crusted Salmon", 
+    "Samuel Tan", 
+    "images/salmon.jpg", 
+    "ingredients.html", 
+    "equipment.html", 
+    "directions.html"
+  );
+
+  Tiramisu = new Recipe(
+    "Classic Tiramisu", 
+    "Alison Roman", 
+    "https://static01.nyt.com/images/2017/04/05/dining/05COOKING-TIRAMISU1/05COOKING-TIRAMISU1-master768.jpg?w=1280&q=75", 
+    "tiramisu-ingredients.html", 
+    "tiramisu-equipment.html", 
+    "tiramisu-directions.html"
+  );
+
+  Burger = new Recipe(
+    "Smashed Avocado-Chicken Burgers", 
+    "Yasmin Fahr", 
+    "https://static01.nyt.com/images/2022/04/18/dining/yf-chicken-avocado-burgers/merlin_205536372_0d3102af-f082-4900-bfd2-176105d53a42-articleLarge.jpg?w=1280&q=75", 
+    "burger-ingredients.html", 
+    "burger-equipment.html", 
+    "burger-directions.html"
+  );
+
+  Pesto = new Recipe(
+    "Pesto Pasta",
+    "John Doe",
+    "images/pesto.jpg",
+    "pesto-ingredients.html",
+    "pesto-directions.html"
+  );
+
+  Reuben = new Recipe(
+    "Reuben Sandwich",
+    "Colette G.",
+    "images/reuben.jpg",
+    "reuben-ingredients.html",
+    "reuben-directions.html"
+  );
+
+  Tomato = new Recipe(
+    "Tomato Soup",
+    "Charlotte",
+    "images/tomato.jpg",
+    "tomato-ingredients.html",
+    "tomato-directions.html"
+  );
+
+  Chicken = new Recipe(
+    "Chicken Salad",
+    "Jackie M.",
+    "images/chicken.jpg",
+    "chicken-ingredients.html",
+    "chicken-directions.html"
+  );
+ 
+window.onload = function() {
+
+  document.querySelector("#firstRecipe").onclick = function() {
+    LemonPankoCrustedSalmon.displayRecipe();
+  }
+  document.querySelector("#secondRecipe").onclick = function() {
+    Tiramisu.displayRecipe();
+  }
+  document.querySelector("#thirdRecipe").onclick = function() {
+    Burger.displayRecipe();
+  }
+  document.querySelector("#fourthRecipe").onclick = function() {
+    Pesto.displayRecipe();
+  }
+  document.querySelector("#fifthRecipe").onclick = function() {
+    Reuben.displayRecipe();
+  }
+  document.querySelector("#sixthRecipe").onclick = function() {
+    Tomato.displayRecipe();
+  }
+  document.querySelector("#seventhRecipe").onclick = function() {
+    Chicken.displayRecipe();
+  }
+
+}
+
+// end window.onload
